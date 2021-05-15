6a0a37fb20da7914a11f7857c1cc41cede6d97b7
Alexandre Mazoyer <mazoyeralexandre@hotmail.fr>
2021-05-15 15:58:12 +0200

    java1

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+class HelloWorld { 
+    
+    public static void main(String[] args) {
+    
+int money = 600;
+
+int voiture = 500;
+
+boolean quete = true;
+
+if(money >= voiture && quete){
+     System.out.println("tu peux" + " WelloWorld");
+}
+
+else if(!quete){
+     System.out.println("tu na pas fait la quete ");
+}
+
+else if(money <= voiture && quete){
+    System.out.println("tu na pas assez de money ");
+}
+
+
+}
+
+}
